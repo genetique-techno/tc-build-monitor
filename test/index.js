13d67b5d6fd55e33ff63fd943c8e61f8eafe13c4
Aaron Todd <aaron.todd@gmail.com>
2017-11-07 22:32:37 -0500

    Test mocks and helpers

--- test/index.js
+++ test/index.js
@@ -1,9 +1,22 @@
 global.assert = require("assert");
 global.sinon = require("sinon");
 global.path = require("path");
+global.proxyquire = require("proxyquire").noCallThru();
+global.r = require("ramda");
 
 // requires a project module by passing a path array relative to the project root
-global.getProjectModule = pathsArr => require(path.resolve.apply(null, [__dirname, ".."].concat(pathsArr)));
+global.getProjectModule = (pathsArr, stubObj) => {
+  if (stubObj) return proxyquire(path.resolve.apply(null, [__dirname, ".."].concat(pathsArr)), stubObj)
+  else return require(path.resolve.apply(null, [__dirname, ".."].concat(pathsArr)))
+};
+
+global.mockMidiHelpers = {
+  // clearAll: (port) => mapOverAllPads(port, 0),
+  // fullColorMap: fullColorMap,
+  // strobeOn: strobeOn,
+  // strobeOff: strobeOff,
+  getKey: x => 1,
+  getVelocity: x => x,
+};
 
 require("./lib");
-
